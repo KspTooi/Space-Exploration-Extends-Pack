ff7927195c09d5e7ba84d63df0670299e64ef016
KspTooi <ksptooi@outlook.com>
2022-01-06 17:53:43 +0800

    增加欢迎文本功能

--- control.lua
+++ control.lua
@@ -11,8 +11,17 @@ end)
 
 
 script.on_event(defines.events.on_player_joined_game, function(event)
+
     local player = game.players[event.player_index]
     player.print("当前KSCP版本:"..version)
+    player.print("欢迎来到特色太空!"..player.name)
+
+    local msg = settings.global["kscp-player-join-message"].value;
+
+    if msg~=nil then
+        player.print(msg)
+    end
+
 end)
 
 
---
+++ settings.lua
@@ -0,0 +1,11 @@
+data:extend{
+  -- MAP SETTINGS
+  {
+    type = "string-setting",
+    name = "kscp-player-join-message",
+    setting_type = "runtime-global",
+    default_value = '',
+    allow_blank = true,
+    order = "aa"
+  }
+}
