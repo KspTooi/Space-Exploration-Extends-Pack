b0d9f8aec97a39be7bdb9796a1e8de1bfb5b5b85
KspTooi <ksptooi@outlook.com>
2022-01-10 11:48:01 +0800

    1.4.12 Updated

--- control.lua
+++ control.lua
@@ -3,7 +3,7 @@
 
 
 local ctx = "KspTooi"
-local version = "1.4C"
+local version = "1.4D"
 
 script.on_event(defines.events.on_player_created, function(event)
     local player = game.players[event.player_index]
