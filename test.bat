db1446c135b7056c6d1d06356641ee3115cb75c9
KspTooi <ksptooi@outlook.com>
2022-01-19 22:24:33 +0800

    测试构建脚本

---
+++ test.bat
@@ -0,0 +1,14 @@
+echo off
+
+set buildTarget="C:\Users\Administrator\AppData\Roaming\Factorio\mods\ksptooi-server-commons-preprocess_1.4.31"
+set projectPath="C:\InternalDeveloper\Lua\ksptooi-server-commons-preprocess"
+
+
+cd %projectPath%
+xcopy /s /y %projectPath% %buildTarget%
+del /F /S /Q "%buildTarget%/.git"
+del /F /S /Q "%buildTarget%/.idea"
+echo 构建完成
+echo 构建完成
+echo 构建完成
+pause
