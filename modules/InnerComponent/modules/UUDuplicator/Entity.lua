21dc7a8134080ad99fc48d6344bdfd2ca0c4d3d9
KspTooi <ksptooi@outlook.com>
2022-01-26 11:11:36 +0800

    fix

--- modules/InnerComponent/modules/UUDuplicator/Entity.lua
+++ modules/InnerComponent/modules/UUDuplicator/Entity.lua
@@ -8,8 +8,8 @@ aspectService.create(TypeEnum.recipeCategory,db.forName("uu-duplicator-category"
 
 
 
-aspectConverter.toAssemblingMachine(aspectService.create(TypeEnum.assemblingMachine)
-        
+aspectConverter.toAssemblingMachine(aspectService.create(TypeEnum.assemblingMachine))
+
         :setName(db.forName("uu-duplicator"))
         :setEntityCollisionMask(ConstEnum.collisionMaskAllowInGroundAndSpace)
         :setAllowInSpace(true)
