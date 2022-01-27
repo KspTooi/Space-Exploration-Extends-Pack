80810431bc4f66a91e8eddc866c722c5369b134c
KspTooi <ksptooi@outlook.com>
2022-01-27 11:15:34 +0800

    采用新模块加载方式

--- starter/ModulesStandardStarter.lua
+++ starter/ModulesStandardStarter.lua
@@ -24,7 +24,10 @@ local modulesInitConfig = {
 ---模块定义
 local modulesDefine = {
 
-    { name = "UUBase" , desc = "UU液体基础" ,  packName = db.forInnerModules("UUBase") }
+    { name = "M1" , desc = "UU液体基础" ,  packName = db.forInnerModules("UUBase") },
+    { name = "M2" , desc = "UU工厂" ,  packName = db.forInnerModules("UUMatterFabricator") },
+    { name = "M3" , desc = "UU复制机" ,  packName = db.forInnerModules("UUDuplicator") },
+    { name = "M4" , desc = "四号组装机" ,  packName = db.forInnerModules("AssemblingT4") }
 
 }
 
@@ -56,19 +59,19 @@ require("modules.InnerComponent.modules.UUBase.Technology")]]
 
 
 --UU物质生成机
-require("modules.InnerComponent.modules.UUMatterFabricator.Entity")
+--[[require("modules.InnerComponent.modules.UUMatterFabricator.Entity")
 require("modules.InnerComponent.modules.UUMatterFabricator.Item")
 require("modules.InnerComponent.modules.UUMatterFabricator.Recipe")
-require("modules.InnerComponent.modules.UUMatterFabricator.Technology")
+require("modules.InnerComponent.modules.UUMatterFabricator.Technology")]]
 
 --UU物质复制机
-require("modules.InnerComponent.modules.UUDuplicator.Entity")
+--[[require("modules.InnerComponent.modules.UUDuplicator.Entity")
 require("modules.InnerComponent.modules.UUDuplicator.Item")
 require("modules.InnerComponent.modules.UUDuplicator.Recipe")
-require("modules.InnerComponent.modules.UUDuplicator.Technology")
+require("modules.InnerComponent.modules.UUDuplicator.Technology")]]
 
 --4号组装机
-require("modules.InnerComponent.modules.Entity")
+--[[require("modules.InnerComponent.modules.Entity")
 require("modules.InnerComponent.modules.Item")
 require("modules.InnerComponent.modules.Recipe")
-require("modules.InnerComponent.modules.Technology")
+require("modules.InnerComponent.modules.Technology")]]
